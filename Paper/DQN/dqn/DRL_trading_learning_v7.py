0e554ab8e35d024c9f18367ce8a89a4d7ddfbf2c
Daytona <Daytona@ipmac-43>
2016-06-08 17:06:45 +1000

    v7 optimizer test adadelta

--- Paper/DQN/dqn/DRL_trading_learning_v7.py
+++ Paper/DQN/dqn/DRL_trading_learning_v7.py
@@ -166,7 +166,7 @@ def run():
     discount_rate = 0.000009
     step_size = 10 # iterate step to update target_model
     act_function = "relu"
-    comment = "Learning rate test (Adam)"
+    comment = "Learning rate test (Adadelta)"
     
     #frame_skip = 4 # train the model with some frames intervals
     input_data = "GBPUSD240.csv"
@@ -218,8 +218,9 @@ def run():
     model.add(Dense(num_actions))
     #RMSprop = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08)
     #model.compile(RMSprop, "mse")
-    Adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    model.compile(Adam,"mse")
+    #Adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    adadelta = keras.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08)
+    model.compile(adadelta,"mse")
     #model.compile(sgd(lr=learning_rate), "mse")
     
     write_model(model, version, time_start)
